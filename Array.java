c081d2fab9b204b9d61ab2fd40190c38ab564ee2
Swapnaroop <swapnaroop2001@gmail.com>
2024-02-01 21:40:07 -0500

    selection , bubble sort

--- Array.java
+++ Array.java
@@ -19,10 +19,42 @@ public class Array {
         }
     }
 
+    public void selectionSort(int arr[]){
+        for (int i = 0; i < arr.length-1; i++) {
+            int mini=i;
+            int key= arr[mini];
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j]<arr[mini]) {
+                    mini=j;
+                }
+            }
+            arr[i]=arr[mini];
+            arr[mini]=key;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public void bubbleSort(int arr[]) {
+        for (int i =arr.length-1; i >0 ; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j]>arr[j+1]) {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
  public static void main(String[] args) {
-    int [] arr={3,4,1,2,9,7};
+    int [] arr={5,4,6,7,3,9,2,7};
     Array a=new Array();
-    a.InsertionSort(arr);
+    a.bubbleSort(arr);;
  }
   
 }
