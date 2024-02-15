298574b1bec12d51b839e75924dcc21f2f663c47
Swapnaroop <swapnaroop2001@gmail.com>
2024-02-14 19:53:38 -0500

    .

--- Array.java
+++ Array.java
@@ -1,6 +1,9 @@
 /**
  * Array
  */
+
+
+//Insertion sort
 public class Array {
     int k;
     public void InsertionSort(int [] arr){
@@ -19,7 +22,8 @@ public class Array {
         }
     }
 
-    public void selectionSort(int arr[]){
+//Selection sort
+public void selectionSort(int arr[]){
         for (int i = 0; i < arr.length-1; i++) {
             int mini=i;
             int key= arr[mini];
@@ -36,7 +40,8 @@ public class Array {
         }
     }
 
-    public void bubbleSort(int arr[]) {
+//Bubble sort
+public void bubbleSort(int arr[]) {
         for (int i =arr.length-1; i >0 ; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j]>arr[j+1]) {
@@ -49,12 +54,55 @@ public class Array {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+" ");
         }
+}
+
+//Merge Sort
+
+public void merge(int arr[],int low,int mid,int high){
+    int idx1=0;
+    int idx2=mid+1;
+    int ans[]=new int[high];
+    int k=0;
+
+    while(idx1<=mid && idx2<=high) {
+        if (arr[idx1]<=arr[idx2]) {
+            ans[k]=arr[idx1];
+            k++;
+            idx1++;
+        }else{
+            ans[k]=arr[idx2];
+            k++;
+            idx2++;
+        }
     }
+
+    while (idx1<=mid) {
+        ans[k++]=arr[idx1++];
+    }
+    while (idx2<=high) {
+        ans[k++]=arr[idx2++];
+    }
+
+    for (int i = 0; i < ans.length; i++) {
+        System.out.print(ans[i]+" ");
+    }
+
+}
+
+public void sort(int arr[],int low, int high){
+    int mid=low+(high-low)/2;
+    sort(arr, low, mid);
+    sort(arr, mid+1, high);
+    merge(arr,low,mid,high);
+}
+
 
  public static void main(String[] args) {
     int [] arr={5,4,6,7,3,9,2,7};
     Array a=new Array();
-    a.bubbleSort(arr);;
+    a.bubbleSort(arr);
+
+    a.sort(arr, 0, 7);
  }
   
 }
