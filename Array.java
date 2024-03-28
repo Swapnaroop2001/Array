e0b7fb1fc751f939cc91c9781a296408fac62674
Swapnaroop <swapnaroop2001@gmail.com>
2024-03-28 11:26:21 -0400

    //Merge 2 sorted Arrays

--- Array.java
+++ Array.java
@@ -910,8 +910,15 @@ public class Array {
         return row;
     }
 
-    
-    
+    //Merge 2 sorted Arrays
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
+
     public static void main(String[] args) {
         int arr2[] = { 1, 1, 3, 2, 2, 3, 5 };
         System.out.println(maxProfit2(arr2));
